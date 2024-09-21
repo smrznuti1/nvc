96ab6a2891940eceb421d866bd1b7be81d3b6354
smrznuti1 <filipbozovic998@gmail.com>
2024-09-21 19:34:20 +0200

    null-ls update

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -27,7 +27,7 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
+        -- null_ls.builtins.completion.spell,
       },
     }
   end,
