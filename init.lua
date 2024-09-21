1361fc448ed208b5b4f81a1cc3c2b59d669fae58
smrznuti1 <filipbozovic998@gmail.com>
2024-09-21 19:31:01 +0200

    Buffer completion

--- init.lua
+++ init.lua
@@ -744,6 +744,7 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
     },
     config = function()
       -- See `:help cmp`
@@ -819,6 +820,7 @@ require('lazy').setup({
           },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'buffer' },
           { name = 'path' },
         },
       }
