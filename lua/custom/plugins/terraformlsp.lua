6c204478557874737c2a72211d983c419a9a811e
smrznuti1 <filipbozovic998@gmail.com>
2024-10-04 22:42:54 +0200

    Feat: Terraform LSP

---
+++ lua/custom/plugins/terraformlsp.lua
@@ -0,0 +1,13 @@
+local lspconfig = require 'lspconfig'
+local cmp = require 'cmp_nvim_lsp'
+local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+lsp_capabilities = vim.tbl_deep_extend('force', lsp_capabilities, cmp.default_capabilities())
+lspconfig.terraform_lsp.setup {
+  filetypes = { 'hcl', 'tf', 'tfvars' },
+  root_dir = function(fname)
+    return lspconfig.util.root_pattern('.terraform', '.git')(fname) or lspconfig.util.path.dirname(fname)
+  end,
+  capabilities = lsp_capabilities,
+}
+
+return {}
