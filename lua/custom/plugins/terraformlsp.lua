bdbb579c07a3635fa2dd144e5356bdc71c031405
smrznuti1 <filipbozovic998@gmail.com>
2024-10-06 14:40:04 +0200

    Fix: Terraform LSP missing command

--- lua/custom/plugins/terraformlsp.lua
+++ lua/custom/plugins/terraformlsp.lua
@@ -3,7 +3,8 @@ local cmp = require 'cmp_nvim_lsp'
 local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
 lsp_capabilities = vim.tbl_deep_extend('force', lsp_capabilities, cmp.default_capabilities())
 lspconfig.terraform_lsp.setup {
-  filetypes = { 'hcl', 'tf', 'tfvars' },
+  cmd= { 'terraform-lsp' },
+  filetypes = { 'hcl', 'tf', 'tfvars', 'terraform' },
   root_dir = function(fname)
     return lspconfig.util.root_pattern('.terraform', '.git')(fname) or lspconfig.util.path.dirname(fname)
   end,
