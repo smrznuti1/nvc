0c5372b63a2a30477ed62d472f9f768e8658f7db
Smrznuti <filipbozovic998@gmail.com>
2024-10-10 18:27:12 +0200

    Fix: Catpuccin Name

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,6 +1,7 @@
 return {
   'catppuccin/nvim',
   lazy = false,
+  name = 'catppuccin',
   config = function()
     require('catppuccin').setup {
       flavour = 'auto', -- latte, frappe, macchiato, mocha
