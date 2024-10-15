e45de04eb72d0deac266958404a55fb68c5f8e49
SESA719972 <filip.bozovic@non.se.com>
2024-10-15 15:20:39 +0200

    New Alpha

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -1,12 +1,6 @@
 return {
   'goolord/alpha-nvim',
-  dependencies = { 'echasnovski/mini.icons' },
-  -- dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local startify = require 'alpha.themes.startify'
-    -- available: devicons, mini, default is mini
-    -- if provider not loaded and enabled is true, it will try to use another provider
-    startify.file_icons.provider = 'mini'
-    require('alpha').setup(startify.config)
+    require('alpha').setup(require('alpha.themes.dashboard').config)
   end,
 }
