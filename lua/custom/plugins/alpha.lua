c66232915e3db55f2db069954e8ff6030b817301
SESA719972 <filip.bozovic@non.se.com>
2024-10-15 15:04:42 +0200

    Mini icosn

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -6,7 +6,7 @@ return {
     local startify = require 'alpha.themes.startify'
     -- available: devicons, mini, default is mini
     -- if provider not loaded and enabled is true, it will try to use another provider
-    startify.file_icons.provider = 'devicons'
+    startify.file_icons.provider = 'mini'
     require('alpha').setup(startify.config)
   end,
 }
