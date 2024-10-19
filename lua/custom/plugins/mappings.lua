53ae6857d15f90f989e703639675d5d0c2c38b29
smrznuti1 <filipbozovic998@gmail.com>
2024-10-19 14:49:59 +0200

    Feat: Floating term

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -99,6 +99,7 @@ vim.keymap.set('t', '<C-h>', '<cmd>wincmd h<cr>', { desc = 'Terminal left window
 vim.keymap.set('t', '<C-j>', '<cmd>wincmd j<cr>', { desc = 'Terminal down window navigation' })
 vim.keymap.set('t', '<C-k>', '<cmd>wincmd k<cr>', { desc = 'Terminal up window navigation' })
 vim.keymap.set('t', '<C-l>', '<cmd>wincmd l<cr>', { desc = 'Terminal right window navigation' })
+vim.keymap.set({ 'n', 't' }, '<M-l>', '<cmd>FloatermToggle<cr>', { silent = true, noremap = true })
 vim.keymap.set('n', '<C-x>', ':sp | te ', {})
 
 -- Highlight
