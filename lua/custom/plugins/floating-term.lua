e4606c2a53564fc7d7fabb37b97c5f7ec1a8df61
SESA719972 <filip.bozovic@non.se.com>
2024-10-21 14:36:06 +0200

    Fix: Better Floating term

--- lua/custom/plugins/floating-term.lua
+++ lua/custom/plugins/floating-term.lua
@@ -1,4 +1,18 @@
 return {
-    'voldikss/vim-floaterm',
-    lazy=false
+  'numToStr/FTerm.nvim',
+  lazy = false,
+  config = function()
+    require('FTerm').setup {
+      border = 'double',
+      dimensions = {
+        height = 0.5,
+        width = 0.9,
+        x = 0.5,
+        y = 1,
+      },
+    }
+
+    vim.api.nvim_create_user_command('FTermOpen', require('FTerm').open, { bang = true })
+vim.api.nvim_create_user_command('FTermClose', require('FTerm').close, { bang = true })
+  end,
 }
