c1be4cab914b1b49e1e5f9cea0a4e1a18a65100d
smrznuti1 <filipbozovic998@gmail.com>
2024-10-26 19:28:40 +0200

    Update sizing

--- lua/custom/plugins/ddgr.lua
+++ lua/custom/plugins/ddgr.lua
@@ -1,19 +1,16 @@
 Ddgr = {}
 Ddgr.__index = Ddgr
 
-function Ddgr:new()
-  local instance = setmetatable({}, Ddgr)
-  instance.buf = nil
+function Ddgr:get_opts()
+  local editor_width = vim.api.nvim_get_option_value('columns', {})
+  local editor_height = vim.api.nvim_get_option_value('lines', {})
 
-  local term_width = vim.o.columns
-  local term_height = vim.o.lines
+  local width = math.floor(editor_width * 0.5) -- 50% of the editor width
+  local height = math.floor(editor_height * 0.8) -- 80% of the editor height
+  local row = math.floor(editor_height / 2) -- Centered vertically
+  local col = math.floor((editor_width - width) / 2) -- Centered horizontally
 
-  local width = math.floor(term_width * 0.5) -- 50% of the terminal width
-  local height = math.floor(term_height * 0.8) -- 20% of the terminal height
-  local row = math.floor((term_height - 2) / 2) -- Centered vertically
-  local col = math.floor((term_width - width) / 2) -- Centered horizontally
-
-  instance.opts = {
+  self.opts = {
     relative = 'editor',
     width = width,
     height = height,
@@ -22,11 +19,18 @@ function Ddgr:new()
     style = 'minimal',
     border = 'single',
   }
+end
+
+function Ddgr:new()
+  local instance = setmetatable({}, Ddgr)
+  instance.buf = nil
+
   return instance
 end
 
 -- Method to get the value
 function Ddgr:openWindow()
+  Ddgr:get_opts()
   if not self.buf or not vim.api.nvim_buf_is_valid(self.buf) then
     self.buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_open_win(self.buf, true, self.opts)
@@ -42,6 +46,6 @@ function OpenDdgr()
   DdgrClass:openWindow()
 end
 
-vim.api.nvim_create_user_command("OpenDdgr", OpenDdgr, {})
+vim.api.nvim_create_user_command('OpenDdgr', OpenDdgr, {})
 
 return {}
