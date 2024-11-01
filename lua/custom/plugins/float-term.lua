2244dbbb89359c21f85612df881c5008f78487fc
SESA719972 <filip.bozovic@non.se.com>
2024-11-01 10:30:36 +0100

    Default values for floaterm

--- lua/custom/plugins/float-term.lua
+++ lua/custom/plugins/float-term.lua
@@ -1,3 +1,11 @@
 return {
   'voldikss/vim-floaterm',
+    config = function ()
+       vim.g.floaterm_height=0.5
+       vim.g.floaterm_width=0.8
+       vim.g.floaterm_wintype='float'
+       vim.g.floaterm_name='cmd'
+       vim.g.floaterm_position='bottom'
+       vim.g.floaterm_autoclose=0
+    end
 }
