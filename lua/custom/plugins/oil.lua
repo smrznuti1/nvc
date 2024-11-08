5269d07b37dfb49a051526c5c94511f8dbfdb76d
Filip Bozovic <sesa719972@non.se.com>
2024-11-08 12:01:15 +0100

    Shortucts

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -68,9 +68,9 @@ return {
         ['<C-s>'] = 'actions.select_vsplit',
         ['<C-h>'] = 'actions.select_split',
         ['<C-t>'] = 'actions.select_tab',
-        ['<C-p>'] = 'actions.preview',
+        ['gp'] = 'actions.preview',
         ['<C-c>'] = 'actions.close',
-        ['<C-l>'] = 'actions.refresh',
+        ['gl'] = 'actions.refresh',
         ['-'] = 'actions.parent',
         ['_'] = 'actions.open_cwd',
         ['`'] = 'actions.cd',
@@ -88,7 +88,7 @@ return {
       use_default_keymaps = true,
       view_options = {
         -- Show files and directories that start with "."
-        show_hidden = false,
+        show_hidden = true,
         -- This function defines what is considered a "hidden" file
         is_hidden_file = function(name, bufnr)
           return vim.startswith(name, '.')
