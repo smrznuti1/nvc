4603288e692f6862500e34b841bfdd892cffd33d
Filip Bozovic <sesa719972@non.se.com>
2024-11-13 13:36:42 +0100

    Open prev buffer

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '<leader>E', ':Neotree toggle<CR>', desc = 'NeoTree reveal', silent = true },
+    -- { '<leader>E', ':Neotree toggle<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
