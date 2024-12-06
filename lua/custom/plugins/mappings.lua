889a0648ca3b19bf21b2299b169866815679a3b4
filipbozovic998@gmail.com <filip@DESKTOP-T5NQDM0.>
2024-12-06 09:15:41 +0100

    Update

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -1,9 +1,9 @@
 -- Funcs
 function completionForRun(arg_lead, cmd_line, cursor_pos)
   local shellcmd_completions = vim.fn.getcompletion(arg_lead, 'shellcmd')
-  local file_completions = vim.fn.getcompletion(arg_lead, 'file_in_path')
+  local file_completions = vim.fn.getcompletion('edit ' .. arg_lead, 'cmdline')
   file_completions = vim.tbl_map(function(item)
-    return item:gsub('([ %%#$])', '`%1')
+    return item:gsub('([ %%#$])', '\\%1')
   end, file_completions)
   return vim.tbl_extend('keep', file_completions, shellcmd_completions)
 end
@@ -83,7 +83,7 @@ _G.change_cwd_to_terminal_path = change_cwd_to_terminal_path
 local builtin = require 'telescope.builtin'
 -- CMD
 vim.api.nvim_create_user_command('Command', function(input)
-  vim.fn.execute(':FloatermNew --height=0.5 --width=0.8 --wintype=float --name=cmd --position=bottom --autoclose=0 ' .. input.args)
+  vim.fn.execute(':FloatermNew --height=0.5 --width=0.8 --wintype=float --name=cmd --position=bottom --autoclose=0 ' .. input.args:gsub('\\', '`'))
 end, { nargs = '*', complete = 'customlist,v:lua.completionForRun' })
 
 -- Workdir
