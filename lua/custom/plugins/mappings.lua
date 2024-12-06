57f5e6e6fb737253a3f278112a83fd0f30129597
Filip Bozovic <filip.bozovic@non.se.com>
2024-12-06 16:42:23 +0100

    Update

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -1,11 +1,16 @@
 -- Funcs
 function completionForRun(arg_lead, cmd_line, cursor_pos)
   local shellcmd_completions = vim.fn.getcompletion(arg_lead, 'shellcmd')
-  local file_completions = vim.fn.getcompletion('edit ' .. arg_lead, 'cmdline')
-  file_completions = vim.tbl_map(function(item)
+
+  shellcmd_completions = vim.tbl_map(function(item)
     return item:gsub('([ ()%%#$])', '\\%1')
-  end, file_completions)
-  return vim.tbl_extend('keep', file_completions, shellcmd_completions)
+  end, shellcmd_completions)
+  -- local file_completions = vim.fn.getcompletion('edit ' .. arg_lead, 'cmdline')
+  -- file_completions = vim.tbl_map(function(item)
+  --   return item:gsub('([ ()%%#$])', '\\%1')
+  -- end, file_completions)
+  -- return vim.tbl_extend('keep', file_completions, shellcmd_completions)
+  return shellcmd_completions
 end
 
 function custom_completion(arg_lead, cmd_line, cursor_pos)
