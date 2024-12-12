459a8e3eed24b7db41d44d9c6d0f4ac6beb3945a
Filip Bozovic <filip.bozovic@non.se.com>
2024-12-12 13:33:44 +0100

    fold

--- lua/custom/plugins/shell.lua
+++ lua/custom/plugins/shell.lua
@@ -15,6 +15,8 @@ vim.api.nvim_create_user_command('NT', 'bd! % | term', {})
 vim.api.nvim_create_user_command('NUT', 'bd! % | Uterm', {})
 vim.api.nvim_create_user_command('CC', 'CopilotChat', {})
 
+vim.opt.foldmethod = 'indent'
+
 -- vim.opt.autochdir = true
 
 -- vim.api.nvim_create_autocmd('BufEnter', {
