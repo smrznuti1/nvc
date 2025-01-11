7f6a85e8ced4c379b6c3ac9696a2e169e0bdaccf
smrznuti1 <filipbozovic998@gmail.com>
2025-01-11 14:55:45 +0100

    update

--- lua/custom/plugins/file-browser.lua
+++ lua/custom/plugins/file-browser.lua
@@ -4,21 +4,24 @@ return {
   -- You don't need to set any of these options.
   -- IMPORTANT!: this is only a showcase of how you can set default options!
   lazy = false,
-  require("telescope").setup({
-    extensions = {
-      file_browser = {
-        theme = "ivy",
-        -- disables netrw and use telescope-file-browser in its place
-        hijack_netrw = true,
-        mappings = {
-          ["i"] = {
-            -- your custom insert mode mappings
-          },
-          ["n"] = {
-            -- your custom normal mode mappings
+  config = function()
+    require("telescope").setup({
+      extensions = {
+        file_browser = {
+          theme = "ivy",
+          -- disables netrw and use telescope-file-browser in its place
+          hijack_netrw = true,
+          mappings = {
+            ["i"] = {
+              -- your custom insert mode mappings
+            },
+            ["n"] = {
+              -- your custom normal mode mappings
+            },
           },
         },
       },
-    },
-  }),
+    })
+    require("telescope").load_extension("file_browser")
+  end,
 }
