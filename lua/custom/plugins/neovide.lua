0cd1a7b986e1b242149b1ee21584cefe214055e5
smrznuti1 <filipbozovic998@gmail.com>
2025-01-12 19:35:04 +0100

    neovide config

---
+++ lua/custom/plugins/neovide.lua
@@ -0,0 +1,22 @@
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 0.8
+  vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h14" -- text below applies for VimScript
+  vim.opt.linespace = 0
+  vim.g.neovide_padding_top = 10
+  vim.g.neovide_padding_bottom = 10
+  vim.g.neovide_padding_right = 10
+  vim.g.neovide_padding_left = 10
+  local alpha = function()
+    return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
+  end
+  -- Transparency
+  vim.g.neovide_transparency = 0.8
+  vim.g.transparency = 1.0
+  -- Blur
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_floating_blur_amount_x = 5.0
+  vim.g.neovide_floating_blur_amount_y = 5.0
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+end
+
+return {}
