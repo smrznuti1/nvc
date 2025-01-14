655df29d3965cc5bdc2c8e7e8c153d5916df7071
smrznuti1 <filipbozovic998@gmail.com>
2025-01-14 07:27:29 +0100

    keybindings

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -6,9 +6,9 @@ return {
   },
   config = function()
     -- require("leap").create_default_mappings()
-    vim.keymap.set({ "n", "x", "o" }, "Z", "<Plug>(leap)")
+    vim.keymap.set({ "n", "x", "o" }, "<M-m>", "<Plug>(leap)")
     -- vim.keymap.set({ "n", "x", "o" }, "zs", "<Plug>(leap-forward)")
     -- vim.keymap.set({ "n", "x", "o" }, "zS", "<Plug>(leap-backward)")
-    vim.keymap.set({ "n", "x", "o" }, "zS", "<Plug>(leap-from-window)")
+    vim.keymap.set({ "n", "x", "o" }, "<M-S-M>", "<Plug>(leap-from-window)")
   end,
 }
