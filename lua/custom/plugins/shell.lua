a5e224909a27b3cb1f5b998311ad468dfdeb74b8
smrznuti1 <filipbozovic998@gmail.com>
2025-01-19 11:22:26 +0100

    command line height

--- lua/custom/plugins/shell.lua
+++ lua/custom/plugins/shell.lua
@@ -1,21 +1,21 @@
-vim.opt.shell = 'pwsh'
+vim.opt.shell = "pwsh"
 vim.opt.shellcmdflag =
   "-NoProfile -NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;"
 vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
 vim.opt.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
-vim.opt.shellquote = ''
-vim.opt.shellxquote = ''
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
 
-vim.api.nvim_create_user_command('Pterm', 'term pwsh', {})
-vim.api.nvim_create_user_command('PT', 'term pwsh', {})
-vim.api.nvim_create_user_command('Uterm', 'term wsl.exe', {})
-vim.api.nvim_create_user_command('UT', 'term wsl.exe', {})
-vim.api.nvim_create_user_command('BDAll', '%bd! | e#', {})
-vim.api.nvim_create_user_command('NT', 'bd! % | term', {})
-vim.api.nvim_create_user_command('NUT', 'bd! % | Uterm', {})
-vim.api.nvim_create_user_command('CC', 'CopilotChat', {})
+vim.api.nvim_create_user_command("Pterm", "term pwsh", {})
+vim.api.nvim_create_user_command("PT", "term pwsh", {})
+vim.api.nvim_create_user_command("Uterm", "term wsl.exe", {})
+vim.api.nvim_create_user_command("UT", "term wsl.exe", {})
+vim.api.nvim_create_user_command("BDAll", "%bd! | e#", {})
+vim.api.nvim_create_user_command("NT", "bd! % | term", {})
+vim.api.nvim_create_user_command("NUT", "bd! % | Uterm", {})
+vim.api.nvim_create_user_command("CC", "CopilotChat", {})
 
-vim.opt.foldmethod = 'indent'
+vim.opt.foldmethod = "indent"
 vim.opt.foldlevelstart = 99
 
 -- vim.opt.autochdir = true
@@ -27,18 +27,18 @@ vim.opt.foldlevelstart = 99
 --   end,
 -- })
 
-vim.api.nvim_create_autocmd('BufEnter', {
-  pattern = '*',
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
   callback = function()
-    if vim.bo.buftype ~= '' then
+    if vim.bo.buftype ~= "" then
       return
     end
     local function find_git_root(path)
-      local git_path = path .. '/.git'
+      local git_path = path .. "/.git"
       if vim.loop.fs_stat(git_path) then
         return path
       end
-      local parent = vim.fn.fnamemodify(path, ':h')
+      local parent = vim.fn.fnamemodify(path, ":h")
       if parent == path then
         return nil
       end
@@ -50,11 +50,11 @@ vim.api.nvim_create_autocmd('BufEnter', {
       if not clients then
         return nil
       end
-      local buffer_path = vim.fn.expand '%:p'
+      local buffer_path = vim.fn.expand("%:p")
       local longest_root_dir = nil
 
       for _, client in pairs(clients) do
-        if client.name == 'null-ls' then
+        if client.name == "null-ls" then
           goto continue
         end
 
@@ -73,16 +73,16 @@ vim.api.nvim_create_autocmd('BufEnter', {
     end
 
     local function set_path()
-      local file_path = vim.fn.expand '%:p:h'
+      local file_path = vim.fn.expand("%:p:h")
       local git_root = find_git_root(file_path)
       local lsp_root = find_lsp_root_dir()
 
       if lsp_root then
-        vim.cmd('silent! lcd ' .. lsp_root)
+        vim.cmd("silent! lcd " .. lsp_root)
       elseif git_root then
-        vim.cmd('silent! lcd ' .. git_root)
+        vim.cmd("silent! lcd " .. git_root)
       else
-        vim.cmd('silent! lcd ' .. file_path)
+        vim.cmd("silent! lcd " .. file_path)
       end
     end
 
@@ -90,11 +90,12 @@ vim.api.nvim_create_autocmd('BufEnter', {
   end,
 })
 
-vim.opt.shortmess = vim.opt.shortmess + 'A'
+vim.opt.shortmess = vim.opt.shortmess + "A"
 
-vim.cmd 'set expandtab'
+vim.cmd("set expandtab")
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+vim.o.cmdheight = 0
 
 return {}
