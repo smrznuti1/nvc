4d87d9ead6024612f12f66a64344bd7cec147ece
smrznuti1 <filipbozovic998@gmail.com>
2025-01-27 13:52:42 +0100

    Azure pipelines

---
+++ lua/custom/plugins/azure-pipelines.lua
@@ -0,0 +1,28 @@
+local cmp = require("cmp_nvim_lsp")
+local lspconfig = require("lspconfig")
+local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+lsp_capabilities = vim.tbl_deep_extend("force", lsp_capabilities, cmp.default_capabilities())
+lspconfig.azure_pipelines_ls.setup({
+  cmd = { "azure-pipelines-language-server", "--stdio" },
+  filetypes = { "yaml", "yml" },
+  root_dir = function(fname)
+    return lspconfig.util.root_pattern(".git")(fname) or vim.fs.dirname(fname)
+  end,
+  single_file_support = true,
+  capabilities = lsp_capabilities,
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+          "pipelines/*.y*l",
+          "/azure-pipeline*.y*l",
+          "/*.azure*",
+          "Azure-Pipelines/**/*.y*l",
+          "Pipelines/*.y*l",
+        },
+      },
+    },
+  },
+})
+
+return {}
