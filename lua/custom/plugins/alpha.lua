63237179a2f16c0ea62d5f74837634c17c181214
smrznuti1 <filipbozovic998@gmail.com>
2025-02-16 20:18:11 +0100

    remove alpha

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -1,39 +1,39 @@
 return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "echasnovski/mini.icons",
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    local dashboard = require("alpha.themes.theta")
-    dashboard.header.val = {
-      "  ███████ ███    ███ ██████  ████████",
-      "  ██      ████  ████ ██   ██     ████",
-      "  ███████ ██ ████ ██ ██████    ████  ",
-      "       ██ ██  ██  ██ ██   ██ ████    ",
-      "  ███████ ██      ██ ██   ██ ████████",
-      " ",
-      "    ███    ██ ██    ██ ██ ███    ███",
-      "    ████   ██ ██    ██ ██ ████  ████",
-      "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-      "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-      "    ██   ████   ████   ██ ██      ██",
-    }
-    -- dashboard.section.header.val = {
-    --   "  ███████ ███    ███ ██████  ████████",
-    --   "  ██      ████  ████ ██   ██     ████",
-    --   "  ███████ ██ ████ ██ ██████    ████  ",
-    --   "       ██ ██  ██  ██ ██   ██ ████    ",
-    --   "  ███████ ██      ██ ██   ██ ████████",
-    --   " ",
-    --   "    ███    ██ ██    ██ ██ ███    ███",
-    --   "    ████   ██ ██    ██ ██ ████  ████",
-    --   "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-    --   "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-    --   "    ██   ████   ████   ██ ██      ██",
-    -- }
-    -- dashboard.section.header.opts.hl = "DashboardHeader"
-    -- dashboard.section.footer.opts.hl = "DashboardFooter"
-    require("alpha").setup(dashboard.config)
-  end,
+  -- "goolord/alpha-nvim",
+  -- dependencies = {
+  --   "echasnovski/mini.icons",
+  --   "nvim-lua/plenary.nvim",
+  -- },
+  -- config = function()
+  --   local dashboard = require("alpha.themes.theta")
+  --   dashboard.header.val = {
+  --     "  ███████ ███    ███ ██████  ████████",
+  --     "  ██      ████  ████ ██   ██     ████",
+  --     "  ███████ ██ ████ ██ ██████    ████  ",
+  --     "       ██ ██  ██  ██ ██   ██ ████    ",
+  --     "  ███████ ██      ██ ██   ██ ████████",
+  --     " ",
+  --     "    ███    ██ ██    ██ ██ ███    ███",
+  --     "    ████   ██ ██    ██ ██ ████  ████",
+  --     "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+  --     "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+  --     "    ██   ████   ████   ██ ██      ██",
+  --   }
+  --   -- dashboard.section.header.val = {
+  --   --   "  ███████ ███    ███ ██████  ████████",
+  --   --   "  ██      ████  ████ ██   ██     ████",
+  --   --   "  ███████ ██ ████ ██ ██████    ████  ",
+  --   --   "       ██ ██  ██  ██ ██   ██ ████    ",
+  --   --   "  ███████ ██      ██ ██   ██ ████████",
+  --   --   " ",
+  --   --   "    ███    ██ ██    ██ ██ ███    ███",
+  --   --   "    ████   ██ ██    ██ ██ ████  ████",
+  --   --   "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+  --   --   "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+  --   --   "    ██   ████   ████   ██ ██      ██",
+  --   -- }
+  --   -- dashboard.section.header.opts.hl = "DashboardHeader"
+  --   -- dashboard.section.footer.opts.hl = "DashboardFooter"
+  --   require("alpha").setup(dashboard.config)
+  -- end,
 }
