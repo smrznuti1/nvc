28c111efe5ff291c70de7664e1fb0d0316969b10
smrznuti1 <filipbozovic998@gmail.com>
2025-02-17 00:21:53 +0100

    custom header

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -5,7 +5,17 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+            preset = {
+      header = [[
+███████ ███    ███ ██████  ████████
+██      ████  ████ ██   ██     ████
+███████ ██ ████ ██ ██████    ████  
+     ██ ██  ██  ██ ██   ██ ████    
+███████ ██      ██ ██   ██ ████████]],
+}
+    },
     explorer = { enabled = false },
     indent = { enabled = true },
     input = { enabled = true },
