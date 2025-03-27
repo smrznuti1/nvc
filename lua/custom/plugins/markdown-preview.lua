9983bd5ec657b412b6b522797431473870bd7ef6
a-fbozovic <filip.bozovic.ext@valeo.com>
2025-03-27 13:02:54 +0100

    markdown preview replacement

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,26 +1,43 @@
+-- return {
+--     "iamcco/markdown-preview.nvim",
+--     ft = "markdown",
+--     build = function()
+--         vim.fn["mkdp#util#install"]()
+--     end,
+--     init = function()
+--         local g = vim.g
+--         g.mkdp_auto_start = 0
+--         g.mkdp_auto_close = 1
+--         g.mkdp_refresh_slow = 0
+--         g.mkdp_command_for_global = 0
+--         g.mkdp_open_to_the_world = 0
+--         g.mkdp_open_ip = ''
+--         g.mkdp_browser = 'firefox'
+--         g.mkdp_echo_preview_url = 0
+--         g.mkdp_browserfunc = ''
+--         g.mkdp_theme = 'dark'
+--         g.mkdp_filetypes = { "markdown" }
+--         g.mkdp_page_title = "${name}.md"
+--         g.mkdp_preview_options = {
+--             disable_sync_scroll = 0,
+--             disable_filename = 1
+--         }
+--     end,
+-- }
+
 return {
-    "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
-    init = function()
-        local g = vim.g
-        g.mkdp_auto_start = 0
-        g.mkdp_auto_close = 1
-        g.mkdp_refresh_slow = 0
-        g.mkdp_command_for_global = 0
-        g.mkdp_open_to_the_world = 0
-        g.mkdp_open_ip = ''
-        g.mkdp_browser = 'firefox'
-        g.mkdp_echo_preview_url = 0
-        g.mkdp_browserfunc = ''
-        g.mkdp_theme = 'dark'
-        g.mkdp_filetypes = { "markdown" }
-        g.mkdp_page_title = "${name}.md"
-        g.mkdp_preview_options = {
-            disable_sync_scroll = 0,
-            disable_filename = 1
-        }
-    end,
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+  config = function()
+    require("markview").setup({
+      preview = {
+        icon_provider = "devicons", -- "mini" or "devicons"
+      },
+    })
+  end,
+  -- For blink.cmp's completion
+  -- source
+  -- dependencies = {
+  --     "saghen/blink.cmp"
+  -- },
 }
