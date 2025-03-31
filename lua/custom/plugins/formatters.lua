5c6d505172e20c4e3065fcb0e9415b56e2c6ec52
a-fbozovic <filip.bozovic.ext@valeo.com>
2025-03-31 09:16:54 +0200

    formatters update

--- lua/custom/plugins/formatters.lua
+++ lua/custom/plugins/formatters.lua
@@ -1,7 +1,8 @@
-require('conform').setup {
+require("conform").setup({
   formatters_by_ft = {
-    terraform = { 'terraform_fmt' },
+    terraform = { "terraform_fmt" },
+    json = { "fixjson" },
   },
-}
+})
 
 return {}
