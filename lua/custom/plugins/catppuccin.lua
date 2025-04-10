04f53a488b894b7908bdadb415e5f81b95b4b110
fbozovic <filip.bozovic.ext@valeo.com>
2025-04-10 10:37:35 +0200

    change coloscheme to tokyonight-night

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,101 +1,101 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  config = function()
-    local configuration = {
-      flavour = "auto", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = true, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.30, -- percentage of the shade to apply to the inactive window
-      },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-      },
-      color_overrides = {
-        mocha = {
-          red = "#ff657a",
-          maroon = "#F29BA7",
-          peach = "#ff9b5e",
-          yellow = "#eccc81",
-          green = "#a8be81",
-          -- teal = "#9cd1bb",
-          -- sky = "#A6C9E5",
-          -- sapphire = "#86AACC",
-          -- blue = "#5d81ab",
-          -- lavender = "#66729C",
-          -- mauve = "#b18eab",
-          text = "#fcfcfa",
-          surface2 = "#535763",
-          surface1 = "#3a3d4b",
-          surface0 = "#30303b",
-          base = "#202027",
-          mantle = "#1c1d22",
-          crust = "#171719",
-          -- base = "#000000",
-          -- mantle = "#000000",
-          -- crust = "#000000",
-          lineNumbers = "#696c91",
-          cursorLineNr = "#e6e6ff",
-        },
-      },
-      custom_highlights = {},
-      default_integrations = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = true,
-        mini = {
-          enabled = true,
-          indentscope_color = "",
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-      },
-      highlight_overrides = {
-        mocha = function(mocha)
-          return {
-            LineNr = { fg = mocha.lineNumbers },
-            CursorLineNr = { fg = mocha.cursorLineNr },
-          }
-        end,
-      },
-    }
-
-    require("catppuccin").setup(configuration)
-
-    -- setup must be called before loading
-    vim.cmd.colorscheme("catppuccin")
-
-    vim.api.nvim_create_user_command("TT", function()
-      local catppuccin = require("catppuccin")
-      configuration.transparent_background = not configuration.transparent_background
-      catppuccin.setup(configuration)
-      vim.cmd.colorscheme("catppuccin")
-    end, {})
-  end,
+  -- "catppuccin/nvim",
+  -- lazy = false,
+  -- name = "catppuccin",
+  -- config = function()
+  --   local configuration = {
+  --     flavour = "auto", -- latte, frappe, macchiato, mocha
+  --     background = { -- :h background
+  --       light = "latte",
+  --       dark = "mocha",
+  --     },
+  --     transparent_background = true, -- disables setting the background color.
+  --     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+  --     term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  --     dim_inactive = {
+  --       enabled = false, -- dims the background color of inactive window
+  --       shade = "dark",
+  --       percentage = 0.30, -- percentage of the shade to apply to the inactive window
+  --     },
+  --     no_italic = false, -- Force no italic
+  --     no_bold = false, -- Force no bold
+  --     no_underline = false, -- Force no underline
+  --     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+  --       comments = { "italic" }, -- Change the style of comments
+  --       conditionals = { "italic" },
+  --       loops = {},
+  --       functions = {},
+  --       keywords = {},
+  --       strings = {},
+  --       variables = {},
+  --       numbers = {},
+  --       booleans = {},
+  --       properties = {},
+  --       types = {},
+  --       operators = {},
+  --       -- miscs = {}, -- Uncomment to turn off hard-coded styles
+  --     },
+  --     color_overrides = {
+  --       mocha = {
+  --         red = "#ff657a",
+  --         maroon = "#F29BA7",
+  --         peach = "#ff9b5e",
+  --         yellow = "#eccc81",
+  --         green = "#a8be81",
+  --         -- teal = "#9cd1bb",
+  --         -- sky = "#A6C9E5",
+  --         -- sapphire = "#86AACC",
+  --         -- blue = "#5d81ab",
+  --         -- lavender = "#66729C",
+  --         -- mauve = "#b18eab",
+  --         text = "#fcfcfa",
+  --         surface2 = "#535763",
+  --         surface1 = "#3a3d4b",
+  --         surface0 = "#30303b",
+  --         base = "#202027",
+  --         mantle = "#1c1d22",
+  --         crust = "#171719",
+  --         -- base = "#000000",
+  --         -- mantle = "#000000",
+  --         -- crust = "#000000",
+  --         lineNumbers = "#696c91",
+  --         cursorLineNr = "#e6e6ff",
+  --       },
+  --     },
+  --     custom_highlights = {},
+  --     default_integrations = true,
+  --     integrations = {
+  --       cmp = true,
+  --       gitsigns = true,
+  --       nvimtree = true,
+  --       treesitter = true,
+  --       notify = true,
+  --       mini = {
+  --         enabled = true,
+  --         indentscope_color = "",
+  --       },
+  --       -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  --     },
+  --     highlight_overrides = {
+  --       mocha = function(mocha)
+  --         return {
+  --           LineNr = { fg = mocha.lineNumbers },
+  --           CursorLineNr = { fg = mocha.cursorLineNr },
+  --         }
+  --       end,
+  --     },
+  --   }
+  --
+  --   require("catppuccin").setup(configuration)
+  --
+  --   -- setup must be called before loading
+  --   vim.cmd.colorscheme("catppuccin")
+  --
+  --   vim.api.nvim_create_user_command("TT", function()
+  --     local catppuccin = require("catppuccin")
+  --     configuration.transparent_background = not configuration.transparent_background
+  --     catppuccin.setup(configuration)
+  --     vim.cmd.colorscheme("catppuccin")
+  --   end, {})
+  -- end,
 }
--- lua/custom/plugins/tokyo.lua
+++ lua/custom/plugins/tokyo.lua
@@ -1,1 +1,17 @@
-return { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}, transparent = true }
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local _, _, tokyoconf = require("tokyonight").load()
+    tokyoconf.transparent = true
+    require("tokyonight").setup(tokyoconf)
+    vim.cmd.colorscheme("tokyonight-night")
+
+    vim.api.nvim_create_user_command("TT", function()
+            tokyoconf.transparent = not tokyoconf.transparent
+      require("tokyonight").setup(tokyoconf)
+      vim.cmd.colorscheme("tokyonight-night")
+    end, {})
+  end,
+}
