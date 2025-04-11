c65fa6ac9ce5155a33f892b251d62c5fd13fb672
fbozovic <filip.bozovic.ext@valeo.com>
2025-04-11 14:16:06 +0200

    false transparecy initially

--- lua/custom/plugins/tokyo.lua
+++ lua/custom/plugins/tokyo.lua
@@ -5,7 +5,7 @@ return {
   opts = {},
   config = function()
     local _, _, tokyoconf = require("tokyonight").load()
-    tokyoconf.transparent = true
+    tokyoconf.transparent = false
 
     tokyoconf.on_highlights = function(highlights, colors)
       highlights.LineNr = {
@@ -23,7 +23,7 @@ return {
     vim.cmd.colorscheme("tokyonight-night")
 
     vim.api.nvim_create_user_command("TT", function()
-            tokyoconf.transparent = not tokyoconf.transparent
+      tokyoconf.transparent = not tokyoconf.transparent
       require("tokyonight").setup(tokyoconf)
       vim.cmd.colorscheme("tokyonight-night")
     end, {})
