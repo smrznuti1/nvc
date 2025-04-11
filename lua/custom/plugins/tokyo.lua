e4972ee6fe251fadeba4ed8339cbedf9e9e3ef35
fbozovic <filip.bozovic.ext@valeo.com>
2025-04-11 08:48:15 +0200

    line numbers colors

--- lua/custom/plugins/tokyo.lua
+++ lua/custom/plugins/tokyo.lua
@@ -6,6 +6,19 @@ return {
   config = function()
     local _, _, tokyoconf = require("tokyonight").load()
     tokyoconf.transparent = true
+
+    tokyoconf.on_highlights = function(highlights, colors)
+      highlights.LineNr = {
+        fg = "#4A599C",
+      }
+      highlights.LineNrAbove = {
+        fg = "#4A599C",
+      }
+      highlights.LineNrBelow = {
+        fg = "#4A599C",
+      }
+    end
+
     require("tokyonight").setup(tokyoconf)
     vim.cmd.colorscheme("tokyonight-night")
 
