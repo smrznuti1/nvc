bf083ddb2e0bbbac6519418830162fea16c6cfd5
smrznuti1 <filipbozovic998@gmail.com>
2025-04-16 01:36:25 +0200

    new theme

--- lua/custom/plugins/tokyo.lua
+++ lua/custom/plugins/tokyo.lua
@@ -1,31 +1,65 @@
 return {
-  "folke/tokyonight.nvim",
+  -- "folke/tokyonight.nvim",
+  -- lazy = false,
+  -- priority = 1000,
+  -- opts = {},
+  -- config = function()
+  --   local _, _, tokyoconf = require("tokyonight").load()
+  --   tokyoconf.transparent = false
+  --
+  --   tokyoconf.on_highlights = function(highlights, colors)
+  --     highlights.LineNr = {
+  --       fg = "#4A599C",
+  --     }
+  --     highlights.LineNrAbove = {
+  --       fg = "#4A599C",
+  --     }
+  --     highlights.LineNrBelow = {
+  --       fg = "#4A599C",
+  --     }
+  --   end
+  --
+  --   require("tokyonight").setup(tokyoconf)
+  --   vim.cmd.colorscheme("tokyonight-night")
+  --
+  --   vim.api.nvim_create_user_command("TT", function()
+  --     tokyoconf.transparent = not tokyoconf.transparent
+  --     require("tokyonight").setup(tokyoconf)
+  --     vim.cmd.colorscheme("tokyonight-night")
+  --   end, {})
+  -- end,
+  "comfysage/cuddlefish.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
   config = function()
-    local _, _, tokyoconf = require("tokyonight").load()
-    tokyoconf.transparent = false
+    local configuration = {
+      theme = {
+        accent = "pink",
+      },
+      editor = {
+        transparent_background = false,
+      },
+      style = {
+        tabline = { "reverse" },
+        search = { "italic", "reverse" },
+        incsearch = { "italic", "reverse" },
+        types = { "italic" },
+        keyword = { "italic" },
+        comment = { "italic" },
+      },
+      overrides = function(colors)
+        return {}
+      end,
+    }
 
-    tokyoconf.on_highlights = function(highlights, colors)
-      highlights.LineNr = {
-        fg = "#4A599C",
-      }
-      highlights.LineNrAbove = {
-        fg = "#4A599C",
-      }
-      highlights.LineNrBelow = {
-        fg = "#4A599C",
-      }
-    end
-
-    require("tokyonight").setup(tokyoconf)
-    vim.cmd.colorscheme("tokyonight-night")
+    require("cuddlefish").setup(configuration)
+    vim.cmd.colorscheme("cuddlefish")
 
     vim.api.nvim_create_user_command("TT", function()
-      tokyoconf.transparent = not tokyoconf.transparent
-      require("tokyonight").setup(tokyoconf)
-      vim.cmd.colorscheme("tokyonight-night")
+      configuration.editor.transparent_background = not configuration.editor.transparent_background
+      require("cuddlefish").setup(configuration)
+      vim.cmd.colorscheme("cuddlefish")
     end, {})
+
   end,
 }
