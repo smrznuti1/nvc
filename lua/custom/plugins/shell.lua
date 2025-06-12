4a6a3e9b7d52471ded35d0910e66db470a5a9250
fbozovic <filip.bozovic.ext@valeo.com>
2025-06-12 13:18:43 +0200

    change shell to zsh

--- lua/custom/plugins/shell.lua
+++ lua/custom/plugins/shell.lua
@@ -1,10 +1,11 @@
-vim.opt.shell = "pwsh"
-vim.opt.shellcmdflag =
-  "-NoProfile -NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;"
-vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-vim.opt.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
-vim.opt.shellquote = ""
-vim.opt.shellxquote = ""
+vim.opt.shell = "zsh"
+-- vim.opt.shell = "pwsh"
+-- vim.opt.shellcmdflag =
+--   "-NoProfile -NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;"
+-- vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+-- vim.opt.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+-- vim.opt.shellquote = ""
+-- vim.opt.shellxquote = ""
 
 vim.api.nvim_create_user_command("Pterm", "term pwsh", {})
 vim.api.nvim_create_user_command("PT", "term pwsh", {})
