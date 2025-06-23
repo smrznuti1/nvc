504b8647e29750364424f449a40f5f033f565a52
fbozovic <filip.bozovic.ext@valeo.com>
2025-06-23 09:36:29 +0200

    autochdir on terminal

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -120,9 +120,35 @@ vim.keymap.set("t", "<M-a>", function()
   vim.defer_fn(function()
     -- vim.cmd("tcd " .. vim.fn.getreg("+"))
     vim.api.nvim_set_current_dir(vim.trim(vim.fn.getreg("+")))
+    vim.api.nvim_set_current_dir(vim.trim(vim.fn.getreg("+")))
     vim.cmd("stopinsert")
-  end, 200)
+  end, 100)
 end, { noremap = true, silent = true, desc = "Change to terminal path" })
+
+vim.api.nvim_create_autocmd({ "TermRequest" }, {
+  desc = "Handles OSC 7 dir change requests",
+  callback = function(ev)
+    if string.sub(ev.data.sequence, 1, 4) == "\x1b]7;" then
+      local dir = string.gsub(ev.data.sequence, "\x1b]7;file://[^/]*", "")
+      if vim.fn.isdirectory(dir) == 0 then
+        vim.notify("invalid dir: " .. dir)
+        return
+      end
+      vim.api.nvim_buf_set_var(ev.buf, "osc7_dir", dir)
+      -- if vim.o.autochdir and vim.api.nvim_get_current_buf() == ev.buf then
+      if vim.api.nvim_get_current_buf() == ev.buf then
+        vim.cmd.cd(dir)
+      end
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "DirChanged" }, {
+  callback = function(ev)
+    if vim.b.osc7_dir and vim.fn.isdirectory(vim.b.osc7_dir) == 1 then
+      vim.cmd.cd(vim.b.osc7_dir)
+    end
+  end,
+})
 
 -- vim.api.nvim_set_keymap(
 --   "t",
