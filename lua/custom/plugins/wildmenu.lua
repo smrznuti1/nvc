03c7adad980ee813ea2c4610ade34ae7de2bc333
smrznuti1 <filipbozovic998@gmail.com>
2025-08-02 00:49:04 +0200

    fuzzy

--- lua/custom/plugins/wildmenu.lua
+++ lua/custom/plugins/wildmenu.lua
@@ -42,7 +42,7 @@ return {
       -- see `:help nvim_open_win()` for available options (e.g. 'single', 'double', etc.)
       max_col_num = 5, -- Maximum number of columns to display in the completion window
       min_col_width = 20, -- Minimum width of completion window columns
-      use_matchfuzzy = false, -- Whether or not to use `matchfuzzy()` (see `:help matchfuzzy()`)
+      use_matchfuzzy = true, -- Whether or not to use `matchfuzzy()` (see `:help matchfuzzy()`)
       -- to order completion results
       highlight_selection = true, -- Whether or not to highlight the currently
       -- selected item, not sure why this is an option tbh
