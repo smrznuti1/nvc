8bcba3e8a6f141c39c86a002ce999674ad5a61c5
fbozovic <filip.bozovic.ext@valeo.com>
2025-08-04 15:37:25 +0200

    add file completion

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -17,13 +17,9 @@ function custom_completion(arg_lead, cmd_line, cursor_pos)
   local words = vim.split(cmd_line, "%s+")
   local last_word = words[#words]
   local shellcmd_completions = vim.fn.getcompletion(last_word, "shellcmd")
-  -- local file_completions = vim.fn.getcompletion(last_word, "file")
-  -- local completions = vim.tbl_extend(
-  --   "keep",
-  --   shellcmd_completions
-  --   file_completions
-  -- )
-  local completions = shellcmd_completions
+  local file_completions = vim.fn.getcompletion(last_word, "file")
+  local completions = vim.tbl_extend("keep", shellcmd_completions, file_completions)
+  -- local completions = shellcmd_completions
 
   completions = vim.tbl_map(function(entry)
     return entry:gsub("[ ()%%#$]", "\\%0")
