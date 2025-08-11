f98576381bbeefa7d37596b5610f50ad683ae559
smrznuti1 <filipbozovic998@gmail.com>
2025-08-11 21:47:28 +0200

    copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,32 +1,33 @@
 return {
-  'zbirenbaum/copilot.lua',
+  "zbirenbaum/copilot.lua",
   lazy = false,
   opts = {
     filetypes = {
-      ['*'] = true,
+      ["*"] = true,
     },
     panel = {
       auto_refresh = false,
       keymap = {
-        accept = '<CR>',
-        jump_prev = '[[',
-        jump_next = ']]',
-        refresh = 'gr',
-        open = '<M-CR>',
+        accept = "<CR>",
+        jump_prev = "[[",
+        jump_next = "]]",
+        refresh = "gr",
+        open = "<M-CR>",
       },
     },
     suggestion = {
-      auto_trigger = true,
+      auto_trigger = false,
       keymap = {
-        accept = '<M-l>',
-        prev = '<M-[>',
-        next = '<M-]>',
+        accept = "<M-l>",
+        prev = "<M-[>",
+        next = "<M-]>",
         -- dismiss = "<C-]>",
       },
     },
   },
   config = function()
-        require('copilot').setup()
-    vim.cmd 'Copilot disable'
+    require("copilot").setup()
+    -- vim.cmd 'Copilot disable'
+    -- vim.fn.execute("Copilot disable", "silent")
   end,
 }
