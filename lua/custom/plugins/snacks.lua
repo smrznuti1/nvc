70356799038818cfef05143d61d8c6f55230d653
fbozovic <filip.bozovic.ext@valeo.com>
2025-08-13 13:45:35 +0200

    cr confirm

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -117,7 +117,7 @@ return {
         keys = {
           n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
           i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
-          i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = { "i", "n" }, expr = true },
+          i_cr = { "<cr>", { "confirm" }, mode = { "i", "n" }, expr = true },
           i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
           i_stab = { "<S-tab>", { "cmp_select_prev", "cmp" }, mode = "i", expr = true },
           i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
