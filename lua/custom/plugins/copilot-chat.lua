4469112f06f6e4ee045e44a94524a00b6278f9b6
smrznuti1 <filipbozovic998@gmail.com>
2025-09-20 17:43:07 +0200

    copilot settings

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -8,10 +8,30 @@ return {
   config = function()
     require("CopilotChat").setup({
       debug = false, -- Enable debugging
+      model = "gpt-4.1",
+      temperature = 0.0,
+      window = {
+        layout = "float",
+        width = 0.8, -- Fixed width in columns
+        height = 0.6, -- Fixed height in rows
+        border = "rounded", -- 'single', 'double', 'rounded', 'solid'
+        title = "AI Assistant",
+        zindex = 100, -- Ensure window stays on top
+      },
+
+      headers = {
+        user = "Filip",
+        assistant = "Copilot",
+        tool = "Tool",
+      },
+
+      separator = "━━",
+      -- auto_fold = true,
+
       -- See Configuration section for rest
       --
       vim.api.nvim_create_user_command("CC", function()
-        vim.cmd("CopilotChat")
+        vim.cmd("CopilotChatToggle")
         vim.cmd("wincmd =")
       end, {}),
       vim.keymap.set(
@@ -19,6 +39,12 @@ return {
         "<leader>cc",
         "<cmd>CC<cr>",
         { desc = "Opet Copilot Chat for current selection", silent = true }
+      ),
+      vim.keymap.set(
+        { "n", "i", "v" },
+        "<M-c>",
+        "<cmd>CC<cr>",
+        { desc = "Opet Copilot Chat", silent = true }
       ),
     })
   end,
