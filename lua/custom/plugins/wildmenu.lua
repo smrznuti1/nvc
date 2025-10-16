182e774802f41a9099d0cbdcb376fb808b5c198c
smrznuti1 <filipbozovic998@gmail.com>
2025-10-16 10:50:33 +0200

    transparanet

--- lua/custom/plugins/wildmenu.lua
+++ lua/custom/plugins/wildmenu.lua
@@ -1,3 +1,67 @@
+return {
+  "gelguy/wilder.nvim",
+  config = function()
+    local wilder = require("wilder")
+    wilder.setup({
+      modes = { ":", "/", "?" },
+      accept_key = "<C-l>",
+      reject_key = "<C-h>",
+      accept_completion_auto_select = 0,
+    })
+
+    wilder.set_option(
+      "renderer",
+      wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+        highlighter = wilder.basic_highlighter(),
+        min_width = "100%", -- minimum height of the popupmenu, can also be a number
+        -- min_height = "50%", -- to set a fixed height, set max_height to the same value
+        reverse = 0,
+        highlights = {
+          border = "Normal", -- highlight to use for the border
+        },
+        -- 'single', 'double', 'rounded' or 'solid'
+        -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+        border = "rounded",
+        pumblend = 40,
+      }))
+    )
+    wilder.set_option("pipeline", {
+      wilder.branch(
+        wilder.cmdline_pipeline({
+          -- sets the language to use, 'vim' and 'python' are supported
+          language = "python",
+          -- 0 turns off fuzzy matching
+          -- 1 turns on fuzzy matching
+          -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
+          -- fuzzy = 1,
+          fuzzy = 2,
+        }),
+        wilder.python_search_pipeline({
+          -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
+          pattern = wilder.python_fuzzy_pattern(),
+          -- omit to get results in the order they appear in the buffer
+          sorter = wilder.python_difflib_sorter(),
+          -- can be set to 're2' for performance, requires pyre2 to be installed
+          -- see :h wilder#python_search() for more details
+          engine = "re",
+        }),
+        wilder.python_file_finder_pipeline({
+          -- to use ripgrep : {'rg', '--files'}
+          -- to use fd      : {'fd', '-tf'}
+          -- file_command = { "find", ".", "-type", "f", "-printf", "%P\n" },
+          file_command = { "fd", "-tf" },
+          -- to use fd      : {'fd', '-td'}
+          -- dir_command = { "find", ".", "-type", "d", "-printf", "%P\n" },
+          dir_command = { "fd", "-td" },
+          -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
+          -- found at https://github.com/nixprime/cpsm
+          filters = { "fuzzy_filter", "difflib_sorter" },
+        })
+      ),
+    })
+  end,
+}
+
 -- return {
 --   "vzze/cmdline.nvim",
 --   lazy = false,
@@ -52,72 +116,3 @@
 --     })
 --   end,
 -- }
-
-return {
-  "gelguy/wilder.nvim",
-  config = function()
-    local wilder = require("wilder")
-    wilder.setup({
-      modes = { ":", "/", "?" },
-      accept_key = "<C-l>",
-      reject_key = "<C-h>",
-      accept_completion_auto_select = 0,
-    })
-
-    wilder.set_option(
-      "renderer",
-      wilder.popupmenu_renderer(
-        wilder.popupmenu_border_theme({
-          highlighter = wilder.basic_highlighter(),
-          min_width = "100%", -- minimum height of the popupmenu, can also be a number
-          -- min_height = "50%", -- to set a fixed height, set max_height to the same value
-          reverse = 0,
-          highlights = {
-            border = "Normal", -- highlight to use for the border
-          },
-          -- 'single', 'double', 'rounded' or 'solid'
-          -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-          border = "rounded",
-        }),
-        {
-          -- highlighter applies highlighting to the candidates
-          highlighter = wilder.basic_highlighter(),
-        }
-      )
-    )
-    wilder.set_option("pipeline", {
-      wilder.branch(
-        wilder.cmdline_pipeline({
-          -- sets the language to use, 'vim' and 'python' are supported
-          language = "python",
-          -- 0 turns off fuzzy matching
-          -- 1 turns on fuzzy matching
-          -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
-          -- fuzzy = 1,
-          fuzzy = 2,
-        }),
-        wilder.python_search_pipeline({
-          -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
-          pattern = wilder.python_fuzzy_pattern(),
-          -- omit to get results in the order they appear in the buffer
-          sorter = wilder.python_difflib_sorter(),
-          -- can be set to 're2' for performance, requires pyre2 to be installed
-          -- see :h wilder#python_search() for more details
-          engine = "re",
-        }),
-        wilder.python_file_finder_pipeline({
-          -- to use ripgrep : {'rg', '--files'}
-          -- to use fd      : {'fd', '-tf'}
-          -- file_command = { "find", ".", "-type", "f", "-printf", "%P\n" },
-          file_command = { "fd", "-tf" },
-          -- to use fd      : {'fd', '-td'}
-          -- dir_command = { "find", ".", "-type", "d", "-printf", "%P\n" },
-          dir_command = { "fd", "-td" },
-          -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
-          -- found at https://github.com/nixprime/cpsm
-          filters = { "fuzzy_filter", "difflib_sorter" },
-        })
-      ),
-    })
-  end,
-}
