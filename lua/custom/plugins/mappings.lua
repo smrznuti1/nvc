d34d1f17a6a513b53c460392a214a29d4f548cf3
smrznuti1 <filipbozovic998@gmail.com>
2025-11-05 23:24:39 +0100

    title command

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -59,7 +59,9 @@ local function executeShellCommand()
     end
     if input ~= "" then
       vim.fn.execute(
-        ":FloatermNew! --height=0.5 --width=0.8 --wintype=float --name=cmd --position=bottom --autoclose=0 "
+        ":FloatermNew! --height=0.5 --width=0.8 --wintype=float --name=cmd --position=bottom --autoclose=0 --title="
+          .. input:gsub("(?<!\\)([()#%%$])", "\\%1"):gsub(" ", "\\ ")
+          .. " "
           .. input:gsub("(?<!\\)([()#%%$])", "\\%1")
           .. "; exit"
       )
@@ -309,59 +311,11 @@ vim.keymap.set({ "n", "t", "i" }, "<M-b>", "<cmd>OpenDdgr<cr>", { noremap = true
 
 -- Terminal & Navigation
 vim.keymap.set("n", "<leader>T", ":term<cr>", { desc = "Terminal Here" })
-vim.keymap.set(
-  { "n", "t", "i" },
-  "<M-t>",
-  "<cmd>FloatermToggle terminal<cr>",
-  { desc = "Terminal Here" }
-)
 vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Terminal left window navigation" })
 vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Terminal down window navigation" })
 vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Terminal up window navigation" })
 vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Terminal right window navigation" })
 
-vim.keymap.set({ "n", "t", "i" }, "<M-l>", function()
-  vim.cmd("FloatermNext")
-  vim.api.nvim_command("stopinsert")
-  local bufname = vim.fn.bufname()
-  if bufname:match("^term://") then
-    local bufname_shorten = bufname:gsub("^term://[^:]*:", ""):gsub(". ~/.zshrc; ", "")
-    local quote_position = string.find(bufname_shorten, "'")
-    if quote_position ~= nil then
-      vim.notify(bufname_shorten:sub(quote_position + 1, #bufname_shorten - 1))
-    else
-      vim.notify(bufname_shorten)
-    end
-  end
-end, { silent = false, noremap = true })
-
-vim.keymap.set({ "n", "t", "i" }, "<M-h>", function()
-  vim.cmd("FloatermPrev")
-  vim.api.nvim_command("stopinsert")
-  local bufname = vim.fn.bufname()
-  if bufname:match("^term://") then
-    local bufname_shorten = bufname:gsub("^term://[^:]*:", ""):gsub(". ~/.zshrc; ", "")
-    vim.notify(bufname_shorten)
-  end
-end, { silent = false, noremap = true })
-
-vim.keymap.set({ "n", "t", "i" }, "<M-e>", function()
-  vim.cmd("FloatermToggle")
-  vim.api.nvim_command("stopinsert")
-
-  local bufname = vim.fn.bufname()
-  if bufname:match("^term://") then
-    local bufname_shorten = bufname:gsub("^term://[^:]*:", ""):gsub(". ~/.zshrc; ", "")
-    local quote_position = string.find(bufname_shorten, "'")
-    if quote_position ~= nil then
-      vim.notify(bufname_shorten:sub(quote_position + 1, #bufname_shorten - 1))
-    else
-      vim.notify(bufname_shorten)
-    end
-  end
-
-  -- vim.notify(vim.fn.bufname())
-end, { silent = true, noremap = true })
 vim.keymap.set({ "n", "i", "t" }, "<C-x>", function()
   vim.api.nvim_feedkeys(
     vim.api.nvim_replace_termcodes("<C-\\><C-n>i", true, false, true),
