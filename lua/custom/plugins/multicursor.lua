affea4a4e7f33ab0c1a298bc826751db8d6dcf80
smrznuti1 <filipbozovic998@gmail.com>
2025-11-14 13:14:32 +0100

    mapping

--- lua/custom/plugins/multicursor.lua
+++ lua/custom/plugins/multicursor.lua
@@ -70,7 +70,7 @@ return {
     end)
 
     -- Align cursor columns.
-    vim.keymap.set("n", "<leader>a", mc.alignCursors)
+    vim.keymap.set("n", "<leader>A", mc.alignCursors)
 
     -- Split visual selections by regex.
     vim.keymap.set("v", "S", mc.splitCursors)
