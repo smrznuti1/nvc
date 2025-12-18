73cde949f9ff0cc1416c1d3957db0ea62e9efd45
smrznuti1 <filipbozovic998@gmail.com>
2025-12-18 13:45:20 +0100

    kubernetes

--- plugin/yamlls.lua
+++ plugin/yamlls.lua
@@ -2,9 +2,8 @@ vim.lsp.config("yamlls", {
   settings = {
     yaml = {
       schemas = {
-        ["kubernetes"] = { "*.yml", "*.yaml" },
+        ["kubernetes"] = { "*-k8s.yml", "*-k8s.yaml" },
       },
     },
   },
 })
-
