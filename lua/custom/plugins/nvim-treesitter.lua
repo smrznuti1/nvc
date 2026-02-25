9df409688603dc5cf551518c82f77375e5d2017b
smrznuti1 <filipbozovic998@gmail.com>
2026-02-25 15:58:48 +0100

    add helm values

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -192,7 +192,9 @@ return {
 
         -- Not every tree-sitter parser is the same as the file type detected
         -- So the patterns need to be registered more cleverly
-        local patterns = {}
+        local patterns = {
+            "yaml.helm-values"
+        }
         for _, parser in ipairs(parsers) do
             local parser_patterns = vim.treesitter.language.get_filetypes(parser)
             for _, pp in pairs(parser_patterns) do
