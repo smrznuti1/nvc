180c28d947a768441ce138a184a891aa28745b2b
Smrznuti <filipbozovic998@gmail.com>
2026-04-16 09:51:20 +0200

    md for vivaldi

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,11 +1,9 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && yarn install',
+  init = function() vim.g.mkdp_filetypes = { 'markdown' } end,
+  ft = { 'markdown' },
   init = function()
     local g = vim.g
     g.mkdp_auto_start = 0
@@ -13,13 +11,13 @@ return {
     g.mkdp_refresh_slow = 0
     g.mkdp_command_for_global = 0
     g.mkdp_open_to_the_world = 0
-    g.mkdp_open_ip = ""
-    g.mkdp_browser = "zen-browser"
+    g.mkdp_open_ip = ''
+    g.mkdp_browser = 'vivaldi'
     g.mkdp_echo_preview_url = 0
-    g.mkdp_browserfunc = ""
-    g.mkdp_theme = "dark"
-    g.mkdp_filetypes = { "markdown" }
-    g.mkdp_page_title = "${name}.md"
+    g.mkdp_browserfunc = ''
+    g.mkdp_theme = 'dark'
+    g.mkdp_filetypes = { 'markdown' }
+    g.mkdp_page_title = '${name}.md'
     g.mkdp_preview_options = {
       disable_sync_scroll = 0,
       disable_filename = 1,
