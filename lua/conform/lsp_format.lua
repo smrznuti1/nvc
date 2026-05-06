6ec3cbb340da3949a32e9b78c74612916d8a1611
Smrznuti <filipbozovic998@gmail.com>
2026-05-06 23:03:35 +0200

    replace deprecated solutioln

--- lua/conform/lsp_format.lua
+++ lua/conform/lsp_format.lua
@@ -1,6 +1,6 @@
 ---This module replaces the default vim.lsp.buf.format() so that we can inject our own logic
-local log = require("conform.log")
-local util = require("vim.lsp.util")
+local log = require 'conform.log'
+local util = require 'vim.lsp.util'
 
 local M = {}
 
@@ -9,17 +9,15 @@ local function apply_text_edits(text_edits, bufnr, offset_encoding, dry_run, und
     #text_edits == 1
     and text_edits[1].range.start.line == 0
     and text_edits[1].range.start.character == 0
-    and text_edits[1].range["end"].line >= vim.api.nvim_buf_line_count(bufnr)
-    and text_edits[1].range["end"].character == 0
+    and text_edits[1].range['end'].line >= vim.api.nvim_buf_line_count(bufnr)
+    and text_edits[1].range['end'].character == 0
   then
     local original_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
-    local new_lines = vim.split(text_edits[1].newText, "\r?\n", {})
+    local new_lines = vim.split(text_edits[1].newText, '\r?\n', {})
     -- If it had a trailing newline, remove it to make the lines match the expected vim format
-    if #new_lines > 1 and new_lines[#new_lines] == "" then
-      table.remove(new_lines)
-    end
-    log.debug("Converting full-file LSP format to piecewise format")
-    return require("conform.runner").apply_format(
+    if #new_lines > 1 and new_lines[#new_lines] == '' then table.remove(new_lines) end
+    log.debug 'Converting full-file LSP format to piecewise format'
+    return require('conform.runner').apply_format(
       bufnr,
       original_lines,
       new_lines,
@@ -31,9 +29,7 @@ local function apply_text_edits(text_edits, bufnr, offset_encoding, dry_run, und
   elseif dry_run then
     return #text_edits > 0
   else
-    if undojoin then
-      vim.cmd.undojoin()
-    end
+    if undojoin then vim.cmd.undojoin() end
     vim.lsp.util.apply_text_edits(text_edits, bufnr, offset_encoding)
     return #text_edits > 0
   end
@@ -42,31 +38,30 @@ end
 ---@param options table
 ---@return table[] clients
 function M.get_format_clients(options)
-  local method = options.range and "textDocument/rangeFormatting" or "textDocument/formatting"
+  local method = options.range and 'textDocument/rangeFormatting' or 'textDocument/formatting'
 
   local clients
   if vim.lsp.get_clients then
-    clients = vim.lsp.get_clients({
+    clients = vim.lsp.get_clients {
       id = options.id,
       bufnr = options.bufnr,
       name = options.name,
       method = method,
-    })
+    }
   else
     ---@diagnostic disable-next-line: deprecated
-    clients = vim.lsp.get_active_clients({
+    clients = vim.lsp.get_active_clients {
       id = options.id,
       bufnr = options.bufnr,
       name = options.name,
-    })
+    }
 
-    clients = vim.tbl_filter(function(client)
-      return client.supports_method(method, { bufnr = options.bufnr })
-    end, clients)
+    clients = vim.tbl_filter(
+      function(client) return client.supports_method(method, { bufnr = options.bufnr }) end,
+      clients
+    )
   end
-  if options.filter then
-    clients = vim.tbl_filter(options.filter, clients)
-  end
+  if options.filter then clients = vim.tbl_filter(options.filter, clients) end
   return clients
 end
 
@@ -74,23 +69,21 @@ end
 ---@param callback fun(err?: string, did_edit?: boolean)
 function M.format(options, callback)
   options = options or {}
-  if not options.bufnr or options.bufnr == 0 then
-    options.bufnr = vim.api.nvim_get_current_buf()
-  end
+  if not options.bufnr or options.bufnr == 0 then options.bufnr = vim.api.nvim_get_current_buf() end
   local bufnr = options.bufnr
   local range = options.range
-  local method = range and "textDocument/rangeFormatting" or "textDocument/formatting"
+  local method = range and 'textDocument/rangeFormatting' or 'textDocument/formatting'
 
   local clients = M.get_format_clients(options)
 
   if #clients == 0 then
-    return callback("[LSP] Format request failed, no matching language servers.")
+    return callback '[LSP] Format request failed, no matching language servers.'
   end
 
   local function set_range(client, params)
     if range then
       local range_params =
-        util.make_given_range_params(range.start, range["end"], bufnr, client.offset_encoding)
+        util.make_given_range_params(range.start, range['end'], bufnr, client.offset_encoding)
       params.range = range_params.range
     end
     return params
@@ -101,29 +94,27 @@ function M.format(options, callback)
     local do_format
     local did_edit = false
     do_format = function(idx, client)
-      if not client then
-        return callback(nil, did_edit)
-      end
+      if not client then return callback(nil, did_edit) end
       local params = set_range(client, util.make_formatting_params(options.formatting_options))
-      local auto_id = vim.api.nvim_create_autocmd("LspDetach", {
+      local auto_id = vim.api.nvim_create_autocmd('LspDetach', {
         buffer = bufnr,
         callback = function(args)
           if args.data.client_id == client.id then
-            log.warn("LSP %s detached during format request", client.name)
-            callback("LSP detached")
+            log.warn('LSP %s detached during format request', client.name)
+            callback 'LSP detached'
           end
         end,
       })
       client.request(method, params, function(err, result, ctx, _)
         vim.api.nvim_del_autocmd(auto_id)
         if not result then
-          return callback(err or "No result returned from LSP formatter")
+          return callback(err or 'No result returned from LSP formatter')
         elseif not vim.api.nvim_buf_is_valid(bufnr) then
-          return callback("buffer was deleted")
-        elseif changedtick ~= require("conform.util").buf_get_changedtick(bufnr) then
+          return callback 'buffer was deleted'
+        elseif changedtick ~= require('conform.util').buf_get_changedtick(bufnr) then
           return callback(
             string.format(
-              "Async LSP formatter discarding changes for %s: concurrent modification",
+              'Async LSP formatter discarding changes for %s: concurrent modification',
               vim.api.nvim_buf_get_name(bufnr)
             )
           )
@@ -152,7 +143,7 @@ function M.format(options, callback)
     local did_edit = false
     for _, client in pairs(clients) do
       local params = set_range(client, util.make_formatting_params(options.formatting_options))
-      local result, err = client.request_sync(method, params, timeout_ms, bufnr)
+      local result, err = client:request_sync(method, params, timeout_ms, bufnr)
       if result and result.result then
         local this_did_edit = apply_text_edits(
           result.result,
@@ -169,9 +160,9 @@ function M.format(options, callback)
         end
       elseif err then
         if not options.quiet then
-          vim.notify(string.format("[LSP][%s] %s", client.name, err), vim.log.levels.WARN)
+          vim.notify(string.format('[LSP][%s] %s', client.name, err), vim.log.levels.WARN)
         end
-        return callback(string.format("[LSP][%s] %s", client.name, err))
+        return callback(string.format('[LSP][%s] %s', client.name, err))
       end
     end
     callback(nil, did_edit)
