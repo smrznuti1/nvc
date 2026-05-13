75e2137bdab88fff224606daa15a870f0b87b74f
Smrznuti <filipbozovic998@gmail.com>
2026-05-13 19:27:50 +0200

    mason lspcofnig

--- lua/custom/plugins/mason-null-ls.lua
+++ lua/custom/plugins/mason-null-ls.lua
@@ -1,6 +1,7 @@
 vim.pack.add {
   'https://github.com/jay-babu/mason-null-ls.nvim',
   'https://github.com/nvimtools/none-ls.nvim',
+  'https://github.com/mason-org/mason-lspconfig.nvim',
 }
 
 require('mason-null-ls').setup {
@@ -27,4 +28,8 @@ require('mason-null-ls').setup {
     'markdownlint',
   },
   handlers = {},
+}
+
+require('mason-lspconfig').setup {
+  automatic_enable = true,
 }
