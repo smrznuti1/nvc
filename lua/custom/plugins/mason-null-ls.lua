509e81debe8eeba5f0cf77459b50c2b0cc392386
Smrznuti <filipbozovic998@gmail.com>
2026-05-13 19:28:38 +0200

    remove duplicate install

--- lua/custom/plugins/mason-null-ls.lua
+++ lua/custom/plugins/mason-null-ls.lua
@@ -1,7 +1,6 @@
 vim.pack.add {
   'https://github.com/jay-babu/mason-null-ls.nvim',
   'https://github.com/nvimtools/none-ls.nvim',
-  'https://github.com/mason-org/mason-lspconfig.nvim',
 }
 
 require('mason-null-ls').setup {
