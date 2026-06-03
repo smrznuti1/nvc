a9b1602fe449cbb86509c5d36bffed9796177d0f
Smrznuti <filipbozovic998@gmail.com>
2026-06-03 10:31:15 +0200

    feat: update gopls

--- plugin/gopls.lua
+++ plugin/gopls.lua
@@ -1,31 +1,3 @@
--- local async = require("lspconfig.async")
--- local util = require("lspconfig.util")
--- local mod_cache = nil
--- local lspconfig = require 'lspconfig'
---
--- lspconfig.gopls.setup({
---   root_dir = function(fname)
---     -- see: https://github.com/neovim/nvim-lspconfig/issues/804
---     if not mod_cache then
---       local result = async.run_command({ "go", "env", "GOMODCACHE" })
---       if result and result[1] then
---         mod_cache = vim.trim(result[1])
---       else
---         mod_cache = vim.fn.system("go env GOMODCACHE")
---       end
---     end
---     if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
---       local clients = util.get_lsp_clients({ name = "gopls" })
---       if #clients > 0 then
---         return clients[#clients].config.root_dir
---       end
---     end
---     return util.root_pattern("go.work", "go.mod", ".git")(fname)
---   end,
---   single_file_support = true,
--- })
---
-
 ---@brief
 ---
 --- https://github.com/golang/tools/tree/master/gopls
@@ -44,87 +16,100 @@ local std_lib = nil
 ---@param custom_args go_dir_custom_args
 ---@param on_complete fun(dir: string | nil)
 local function identify_go_dir(custom_args, on_complete)
-  local cmd = { "go", "env", custom_args.envvar_id }
+  local cmd = { 'go', 'env', custom_args.envvar_id }
   vim.system(cmd, { text = true }, function(output)
-    local res = vim.trim(output.stdout or "")
-    if output.code == 0 and res ~= "" then
-      if custom_args.custom_subdir and custom_args.custom_subdir ~= "" then
+    local res = vim.trim(output.stdout or '')
+    if output.code == 0 and res ~= '' then
+      if custom_args.custom_subdir and custom_args.custom_subdir ~= '' then
         res = res .. custom_args.custom_subdir
       end
       on_complete(res)
     else
-      vim.schedule(function()
-        vim.notify(
-          ("[gopls] identify " .. custom_args.envvar_id .. " dir cmd failed with code %d: %s\n%s"):format(
-            output.code,
-            vim.inspect(cmd),
-            output.stderr
+      vim.schedule(
+        function()
+          vim.notify(
+            ('[gopls] identify ' .. custom_args.envvar_id .. ' dir cmd failed with code %d: %s\n%s'):format(
+              output.code,
+              vim.inspect(cmd),
+              output.stderr
+            ),
+            vim.log.levels.ERROR
           )
-        )
-      end)
+        end
+      )
       on_complete(nil)
     end
   end)
 end
 
----@return string?
-local function get_std_lib_dir()
-  if std_lib and std_lib ~= "" then
-    return std_lib
-  end
-
-  identify_go_dir({ envvar_id = "GOROOT", custom_subdir = "/src" }, function(dir)
-    if dir then
-      std_lib = dir
-    end
+---@param on_complete fun()
+local function ensure_std_lib_dir(on_complete)
+  if std_lib and std_lib ~= '' then return on_complete() end
+  identify_go_dir({ envvar_id = 'GOROOT', custom_subdir = '/src' }, function(dir)
+    if dir then std_lib = dir end
+    on_complete()
   end)
-  return std_lib
 end
 
----@return string?
-local function get_mod_cache_dir()
-  if mod_cache and mod_cache ~= "" then
-    return mod_cache
-  end
-
-  identify_go_dir({ envvar_id = "GOMODCACHE" }, function(dir)
-    if dir then
-      mod_cache = dir
-    end
+---@param on_complete fun()
+local function ensure_mod_cache_dir(on_complete)
+  if mod_cache and mod_cache ~= '' then return on_complete() end
+  identify_go_dir({ envvar_id = 'GOMODCACHE' }, function(dir)
+    if dir then mod_cache = dir end
+    on_complete()
   end)
-  return mod_cache
 end
 
 ---@param fname string
 ---@return string?
 local function get_root_dir(fname)
-  if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
-    local clients = vim.lsp.get_clients({ name = "gopls" })
-    if #clients > 0 then
-      return clients[#clients].config.root_dir
-    end
+  local in_cache = mod_cache and fname:sub(1, #mod_cache) == mod_cache
+  local in_std = std_lib and fname:sub(1, #std_lib) == std_lib
+  if in_cache or in_std then
+    local clients = vim.lsp.get_clients { name = 'gopls' }
+    if #clients > 0 then return clients[#clients].config.root_dir end
   end
-  if std_lib and fname:sub(1, #std_lib) == std_lib then
-    local clients = vim.lsp.get_clients({ name = "gopls" })
-    if #clients > 0 then
-      return clients[#clients].config.root_dir
-    end
-  end
-  return vim.fs.root(fname, "go.work") or vim.fs.root(fname, "go.mod") or vim.fs.root(fname, ".git")
+  return vim.fs.root(fname, { 'go.work', 'go.mod', '.git' })
 end
 
 ---@type vim.lsp.Config
-vim.lsp.config("gopls", {
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+vim.lsp.config('gopls', {
+  cmd = { 'gopls' },
+  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
-    get_mod_cache_dir()
-    get_std_lib_dir()
-    -- see: https://github.com/neovim/nvim-lspconfig/issues/804
-    on_dir(get_root_dir(fname))
+    -- resolve GOMODCACHE + GOROOT first, then compute the root. both lookups
+    -- are async, so chain them and only call on_dir once both are ready.
+    ensure_mod_cache_dir(function()
+      ensure_std_lib_dir(function()
+        vim.schedule(function() on_dir(get_root_dir(fname)) end)
+      end)
+    end)
   end,
+  settings = {
+    gopls = {
+      gofumpt = true,
+      staticcheck = true,
+      completeUnimported = true,
+      usePlaceholders = true,
+      semanticTokens = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+        nilness = true,
+        unusedwrite = true,
+        useany = true,
+      },
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
 })
 
-vim.lsp.enable("gopls")
-
+vim.lsp.enable 'gopls'
