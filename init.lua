447f15d7ea701d57f35ea5bf289a8e561473965a
Smrznuti <filipbozovic998@gmail.com>
2026-06-03 11:31:50 +0200

    remove venv selecto

--- init.lua
+++ init.lua
@@ -272,11 +272,6 @@ do
       local kind = ev.data.kind
       if kind ~= 'install' and kind ~= 'update' then return end
 
-      if name == 'telescope-fzf-native.nvim' and vim.fn.executable 'make' == 1 then
-        run_build(name, { 'make' }, ev.data.path)
-        return
-      end
-
       if name == 'LuaSnip' then
         if vim.fn.has 'win32' ~= 1 and vim.fn.executable 'make' == 1 then
           run_build(name, { 'make', 'install_jsregexp' }, ev.data.path)
