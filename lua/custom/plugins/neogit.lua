3f6ce1b5b3b342d78e8565d9f7d5a096e8a9eab2
Smrznuti <filipbozovic998@gmail.com>
2026-07-01 13:09:43 +0200

    Fix: remove c-x mapping neogit to allow command executions

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -3,4 +3,9 @@ vim.pack.add { 'https://github.com/NeogitOrg/neogit' }
 require('neogit').setup {
   integrations = { codediff = true },
   diff_viewer = 'codediff',
+  mappings = {
+    status = {
+      ['<c-x>'] = false,
+    },
+  },
 }
