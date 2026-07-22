7131bfdaebdb6f05d8abc8b1d99fdd14d6df09b6
Smrznuti <filipbozovic998@gmail.com>
2026-07-22 20:39:07 +0200

    fix(fyler): open in fyler

--- lua/custom/fyler_open.lua
+++ lua/custom/fyler_open.lua
@@ -1,4 +1,7 @@
 return function(root_path)
+  root_path = vim.fn.fnamemodify(root_path or '', ':p')
+  if vim.fn.isdirectory(root_path) == 0 then root_path = vim.fn.getcwd() end
+
   local win_id = vim.api.nvim_get_current_win()
   local instance = require('fyler.finder').instance_get_or_nil()
   if instance and instance.win_id ~= win_id then instance:close() end
