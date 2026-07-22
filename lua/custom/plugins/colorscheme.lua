058d94be3a627fb14cedc1015c93c80d13cb8196
Smrznuti <filipbozovic998@gmail.com>
2026-07-22 13:28:15 +0200

    Revert "fix(coloring): terminal coloring"
    This reverts commit d49ef52c29705e1d4acce2350b4e1022b52d663f.

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -32,37 +32,15 @@ local configuration = {
   end,
 }
 
-local kitty_palette = {
-  '#000000',
-  '#cc0403',
-  '#19cb00',
-  '#cecb00',
-  '#0d73cc',
-  '#cb1ed1',
-  '#0dcdcd',
-  '#dddddd',
-  '#767676',
-  '#f2201f',
-  '#23fd00',
-  '#fffd00',
-  '#1a8fff',
-  '#fd28ff',
-  '#14ffff',
-  '#ffffff',
-}
-
-local function apply()
-  require('cuddlefish').setup(configuration)
-  vim.cmd.colorscheme 'cuddlefish'
-  vim.cmd 'highlight CursorLine cterm=underline gui=underline'
-  for i, color in ipairs(kitty_palette) do
-    vim.g['terminal_color_' .. (i - 1)] = color
-  end
-end
-
-apply()
+require('cuddlefish').setup(configuration)
+vim.cmd.colorscheme 'cuddlefish'
+vim.cmd 'highlight CursorLine cterm=underline gui=underline'
+vim.g.terminal_color_8 = '#81439c'
 
 vim.api.nvim_create_user_command('TT', function()
   configuration.editor.transparent_background = not configuration.editor.transparent_background
-  apply()
+  require('cuddlefish').setup(configuration)
+  vim.cmd.colorscheme 'cuddlefish'
+  vim.cmd 'highlight CursorLine cterm=underline gui=underline'
+  vim.g.terminal_color_8 = '#81439c'
 end, {})
